75aaf0516dade4656725ab862f290b69ffca77f4
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-04-21 03:52:03 +0000

    [fin]アプリケーションに必要なファイルの準備

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def index
+    
+  end
+
+  def create
+    
+  end
+  
+  def show
+    
+  end
+
+  def edit
+    
+  end
+  
+  def destroy
+    
+  end
+  
+end
