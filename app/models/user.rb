8559caa569dd57602689ba797a189a86f1223f58
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-04-21 06:38:51 +0000

    booksでのuser info部分の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,13 @@ class User < ApplicationRecord
   validates :name, length: {minimum:2, maximum:20}
   validates :introduction, length: {maximum:50}
   
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join("app/assets/images/no_image.jpg")
+      profile_image.attach(io: File.open(file_path), filename: "no_image.jpg", content_type: "image/jpeg")
+    else
+      profile_image.variant(resize_to_limit: [width, height]).processed
+    end
+  end
   
 end
