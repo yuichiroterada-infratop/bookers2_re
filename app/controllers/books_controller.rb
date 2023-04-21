890048f5bfc7d3e39c40d04056921847add7ba19
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-04-21 12:50:58 +0000

    投稿詳細画面作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,25 @@
 class BooksController < ApplicationController
   def index
-    
+    @book = Book.new
+    @books = Book.all
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user = current_user
+    if @book.save
+      flash[:notice] = "You have created book successfully!"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
     
   end
   
   def show
-    
+    @book = Book.new
+    @show_book = Book.find(params[:id])
   end
 
   def edit
@@ -17,6 +28,11 @@ class BooksController < ApplicationController
   
   def destroy
     
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
   
 end
